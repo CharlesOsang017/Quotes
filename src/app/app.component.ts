f4ac82ff203939d1d564a17283469e677fb708cc
CharlesOsango17 <charlesosango02@gmail.com>
2020-08-22 11:54:21 +0300

    Displayed an Array

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  quotes: string[];
+
+  constuctor(){
+    this.quotes =['If I have seen further it is by standing on the shoulders of giants.– Sir Isaac Newton', 'The roots of education … are bitter, but the fruit is sweet. – Aristotle', 'I have to study myself in actuality – as I am, not as I wish to be.—KRISHNAMURTI']
+  }
+}
