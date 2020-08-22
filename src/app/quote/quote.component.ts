59c3f6a33c59b1fb26366235f5aecd843604a850
CharlesOsango17 <charlesosango02@gmail.com>
2020-08-22 13:36:08 +0300

    Added an author to the class to display the author of the quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,10 +7,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes : Quote[] = [
-    {id:1, name:'If I have seen further it is by standing on the shoulders of giants.– Sir Isaac Newton'},
-    {id:2, name:'The roots of education … are bitter, but the fruit is sweet. – Aristotle'},
-    {id:1, name:'I have to study myself in actuality – as I am, not as I wish to be.—KRISHNAMURTI'},
-    {id:1, name:'We are rarely proud when we are alone –  Voltaire'},
+    {id:1, name:'If I have seen further it is by standing on the shoulders of giants',author:'Sir Isaac Newton'},
+    {id:2, name:'The roots of education … are bitter, but the fruit is sweet', author:'Aristotle'},
+    {id:1, name:'I have to study myself in actuality – as I am, not as I wish to be.',author:'KRISHNAMURTI'},
+    {id:1, name:'We are rarely proud when we are alone.',author:'Voltaire'},
 ];
   constructor() { }
 
