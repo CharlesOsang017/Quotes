f9fb05e7001abc623cd235ff4697b6f10a5caad3
CharlesOsango17 <charlesosango02@gmail.com>
2020-08-22 16:40:05 +0300

    Added a highlight action to the quote

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,9 +1,24 @@
-import { Directive, ElementRef} from '@angular/core';
-
+import { Directive, ElementRef, HostListener} from '@angular/core';
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-  constructor() { }
 
+    
+  constructor(private elem:ElementRef){}
+
+  @HostListener("click") onClicks(){
+    this.textDeco("underline")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+
+  }
 }
+
+
