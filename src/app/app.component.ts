013770524b040d553d6f2ce5184446fecb5e46cc
CharlesOsango17 <charlesosango02@gmail.com>
2020-08-22 13:13:25 +0300

    Assigned my quotes to different id and name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Quote } from './quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes : string[];
-  constructor(){
-    this.quotes = ['If I have seen further it is by standing on the shoulders of giants.– Sir Isaac Newton', 'The roots of education … are bitter, but the fruit is sweet. – Aristotle', 'I have to study myself in actuality – as I am, not as I wish to be.—KRISHNAMURTI','We are rarely proud when we are alone –  Voltaire']
-  }
+  quotes : Quote[] = [
+    {id:1, name:'If I have seen further it is by standing on the shoulders of giants.– Sir Isaac Newton'},
+    {id:2, name:'The roots of education … are bitter, but the fruit is sweet. – Aristotle'},
+    {id:1, name:'I have to study myself in actuality – as I am, not as I wish to be.—KRISHNAMURTI'},
+    {id:1, name:'We are rarely proud when we are alone –  Voltaire'},
+];
 }
