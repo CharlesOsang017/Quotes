a2b287be031c8c6452ac55c7deaabb56149d3ee7
CharlesOsango17 <charlesosango02@gmail.com>
2020-08-22 13:58:20 +0300

    Changed the quote and added constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,6 @@
 export class Quote {
-    id: number;
-    name: string;
-    author: string;
+   showAthor: boolean;
+   constructor(public id: number,public name: string, public author: string){
+       this.showAthor=false;
+   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,12 +6,12 @@ import { Quote } from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes : Quote[] = [
-    {id:1, name:'If I have seen further it is by standing on the shoulders of giants',author:'Sir Isaac Newton'},
-    {id:2, name:'The roots of education … are bitter, but the fruit is sweet', author:'Aristotle'},
-    {id:1, name:'I have to study myself in actuality – as I am, not as I wish to be.',author:'KRISHNAMURTI'},
-    {id:1, name:'We are rarely proud when we are alone.',author:'Voltaire'},
-];
+  quotes: Quote[] = [
+    new Quote(1, 'If I have seen further it is by standing on the shoulders of giants', 'Sir Isaac Newton'),
+    new Quote(2, 'The roots of education … are bitter, but the fruit is sweet', 'Aristotle'),
+    new Quote(1, 'I have to study myself in actuality – as I am, not as I wish to be.', 'KRISHNAMURTI'),
+    new Quote(1, 'We are rarely proud when we are alone.', 'Voltaire'),
+  ];
   constructor() { }
 
   ngOnInit() {
